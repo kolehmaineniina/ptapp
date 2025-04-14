af3384176f2e3cf6605fcaa8ac836e18c54bc7f7
kolehmaineniina <kolehmaineniina@gmail.com>
2025-04-14 18:39:18 +0300

    Customer AgGrid added: edit and delete actions still to be added after implenting a MUI Dialog for adding new customers

--- src/pages/CustomersPage.tsx
+++ src/pages/CustomersPage.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchCustomers } from '../api/customers';
 import CustomerGrid from '../components/CustomerGrid';
-import { Customer } from '../api/types';
 
 export default function CustomersPage() {
     const { data, isLoading, error, isSuccess } = useQuery({queryKey: ['customers'], queryFn: fetchCustomers});
+    const customers = data?._embedded?.customers ?? [];
 
     return (
         <>
