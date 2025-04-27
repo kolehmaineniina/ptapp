16dbacc2f72a22a248304792ea94ede00fd3ed27
kolehmaineniina <kolehmaineniina@gmail.com>
2025-04-27 13:29:47 +0300

    Typed training data

--- src/api/types.ts
+++ src/api/types.ts
@@ -10,9 +10,28 @@ export interface Customer {
     _links: {
         self: { href: string };
         customer?: { href: string };
-        trainings?: { href: string };
+        trainings: { href: string };
       };
     }
+
+  export interface TrainingCustomer {
+    id: number;
+    firstname: string;
+    lastname: string;
+    streetaddress: string;
+    postcode: string;
+    city: string;
+    email: string;
+    phone: string;
+  }
+
+  export interface Training {
+    id: number;
+    date: string;
+    duration: number;
+    activity: string;
+    customer: TrainingCustomer;
+  }
 
 
   
