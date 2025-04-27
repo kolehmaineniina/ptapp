df135f32127bbeeea9941b45d82dfef02ccb2936
kolehmaineniina <kolehmaineniina@gmail.com>
2025-04-27 13:58:51 +0300

    Made improvements to the the List and added action buttons of which 'Go to' button is not yet functional. PS. Just noticed that the new component wasnt tracked, so most of  the changes mentioned in the last two commits we'll be pushed now

---
+++ src/components/Customers/CustomerDrawer.tsx
@@ -0,0 +1,46 @@
+import { Button, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { Customer, Training } from "../../api/types";
+
+export default function CustomerDrawer({anchor, open, onClose, customer, trainings, isLoading, error}: {
+    anchor: string;
+    open: boolean;
+    onClose: () => void;
+    customer: Customer;
+    trainings: Training[];
+    isLoading: boolean;
+    error: Error | null }) {
+
+    return (
+        <Drawer anchor="right" open={open} onClose={onClose}>
+            <div style={{ width: 400, padding: "1rem" }}>
+                <Typography variant="h5">
+                    {customer.firstname} {customer.lastname}'s Trainings
+                </Typography>
+                <List>
+                    {trainings.map((training: Training) => (
+                        <ListItem key={training.id}>
+                           <ListItemIcon></ListItemIcon>
+                           <ListItemText primary={`${training.activity}`}
+                                secondary={`${training.date}: ${training.duration} minutes`}/>
+                        </ListItem>
+                    ))}
+                </List>
+                <Button
+                    onClick={onClose}
+                    variant="outlined"
+                    style={{ marginTop: "1rem" }}
+                    fullWidth
+                >Close
+                </Button>
+                <Button
+                    color="primary"
+                    variant="contained"
+                    style={{ marginTop: "1rem" }}
+                    fullWidth
+                    onClick={() => {}}
+                >Go to Profile
+                </Button>
+            </div>
+        </Drawer>
+    )
+}
